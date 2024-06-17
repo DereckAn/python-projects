9cfe2c3e1f4bc3e76ae10284e27bf74595fc61dc
DereckAn <dereckangeles15071998@gmail.com>
2024-06-17 02:39:52 -0600

    creating md file

--- crearmd.py
+++ crearmd.py
@@ -4,17 +4,38 @@
 
 import flet as ft
 
+class Example(ft.Column):
+    def __init__(self, example_delete):
+        super().__init__()
+        self.example_delete = example_delete
+        self.example = ft.TextField(label="Example", multiline=True, expand=1)
+        self.display_example = ft.Row(controls=[
+            self.example, 
+            ft.IconButton(
+                ft.icons.DELETE_OUTLINE, 
+                tooltip="Delete Example", 
+                on_click=self.delete_example,
+                icon_color="red",),
+        ])  
+
+        self.controls = [self.display_example]
+    
+    def delete_example(self, e):
+        self.example_delete(self)
+
 class Notes(ft.Column):
     def __init__(self, notes_delete, nnotes=2):
         super().__init__()
+        self.notes_delete = notes_delete
+        self.note = ft.TextField(label=f"Note {nnotes}", multiline=True, expand=1)
         self.display_notes = ft.Row(controls=[
-            ft.TextField(label=f"Note {nnotes}", multiline=True, expand=1), 
+            self.note,
             ft.IconButton(
                 ft.icons.DELETE_OUTLINE, 
                 tooltip=f"Delete Note {nnotes}", 
                 on_click=self.delete_notes,
                 icon_color="red",),])
-        self.notes_delete = notes_delete
+        
         
         self.controls = [self.display_notes]
     
@@ -63,6 +84,7 @@ class Parameter(ft.Column):
     def __init__(self, parameter_name, 
                  parameter_required, 
                  parameter_types,
+                 parameter_description,
                  parameter_null,
                  parameter_default, 
                  parameter_delete):
@@ -73,6 +95,7 @@ class Parameter(ft.Column):
         self.display_parameter_default = ft.Text(parameter_default if parameter_default else "N/A")
         self.display_parameter_null = ft.Text(parameter_null if parameter_null else "Returns `null`")
         self.display_parameter_types = ft.Text(", ".join(parameter_types))
+        self.display_parameter_description = ft.Text(parameter_description)
         self.parameter_delete = parameter_delete
         
         # self.display_task = ft.Checkbox(
@@ -109,6 +132,7 @@ class Parameter(ft.Column):
                 self.display_parameter_name,
                 self.display_parameter_required,
                 self.display_parameter_types,
+                self.display_parameter_description,
                 self.display_parameter_default,
                 self.display_parameter_null,
                 ft.Row(
@@ -180,8 +204,8 @@ class MakerMarkerDown(ft.Column):
         super().__init__()
         
         self.vnum = 1
-        self.nnotes =1
-        self.width = 800
+        self.nnotes = 0
+        self.width = 1000
         self.param_types = ["String", "Col<String>", "Double", "Col<Double>", "Int32", "Col<Int32>"]
         
         self.func_title = self.create_text_field("Title of the Function", "TITLE")
@@ -194,6 +218,7 @@ class MakerMarkerDown(ft.Column):
                 ft.DataColumn(ft.Text("Parameter")),
                 ft.DataColumn(ft.Text("Required")),
                 ft.DataColumn(ft.Text("Type(s)")),
+                ft.DataColumn(ft.Text("Description")),
                 ft.DataColumn(ft.Text("`null` Behavior")),
                 ft.DataColumn(ft.Text("Default")),
                 ft.DataColumn(ft.Text("Edit")),
@@ -201,9 +226,13 @@ class MakerMarkerDown(ft.Column):
             rows=[],
         )
         self.parameters = ft.Column()
-        self.versions = ft.Column(
-            controls=[
-                ft.Container(content=ft.Column(
+        self.versions = ft.Column()
+        self.notes=ft.Column()
+        self.examples = ft.Column()
+        self.select = ft.Checkbox(label="SELECT", value=True)
+        self.where = ft.Checkbox(label="WHERE", value=True)
+        self.custom_usage = ft.TextField(label="Default SELECT/WHERE",hint_text="may be used in the query SELECT and WHERE clauses for analyzing data and applying conditional logic.")
+        self.version1 = ft.Container(content=ft.Column(
                     controls=[
                         self.titlee("Version 1", "Blue"),
                         ft.Row(
@@ -215,13 +244,11 @@ class MakerMarkerDown(ft.Column):
                         ft.Row( controls=[self.func_description,]),
                             ]
                         ))
-            ]
-        )
-        self.notes=ft.Column()
         
         self.parameter_name = ft.TextField(width=125, label="Parameter")
-        self.parameter_required = ft.Checkbox( label="Required")
+        self.parameter_required = ft.Checkbox( label="")
         self.parameter_types = []
+        self.parameter_description = ft.TextField(label="Description")
         self.parameter_null = ft.TextField(width=150, label="Null Behavior")
         self.parameter_default = ft.TextField(width=150, label="Default")
         self.checkboxes = {}
@@ -240,9 +267,9 @@ class MakerMarkerDown(ft.Column):
                 ],
             ),
             ft.Divider(height=1, color="gray"),
+            self.version1,
             self.versions,
-            ft.CupertinoFilledButton(text="Add Version", on_click=self.add_version, padding=10,),
-            ft.Divider(height=1, color="white"),
+            ft.FilledTonalButton("Version", icon="ad    d", on_click=self.add_version),
             self.titlee("Parameters", "blue"),
             self.table,
             ft.Divider(height=1, color="gray"),
@@ -268,38 +295,47 @@ class MakerMarkerDown(ft.Column):
                             ]
                         )
                     ),
+                    self.parameter_description,
                     self.parameter_null,
                     self.parameter_default,
                 ]),
-            ft.CupertinoFilledButton(text="Add Parameter", on_click=self.add_parameter, padding=10, ),
-            ft.Divider(height=1, color="white"),
+            ft.FilledTonalButton("Parameter", icon="add", on_click=self.add_parameter),
             self.titlee("Query Builder", "blue"),
             ft.Row(
                 alignment=ft.MainAxisAlignment.SPACE_BETWEEN,  # Alineación para distribuir los controles
                 controls=[
                     ft.Row(  # Columna para los checkboxes
                         controls=[
-                            ft.Checkbox(label="SELECT", value=True),
-                            ft.Checkbox(label="WHERE", value=False),
+                            self.select,
+                            self.where
                         ],
                     ),
-                    ft.TextField(label="Default SELECT/WHERE",hint_text="may be used in the query SELECT and WHERE clauses for analyzing data and applying conditional logic."),  # TextField a la derecha
+                    self.custom_usage,
                 ],
             ),  
-            ft.Divider(height=1, color="white"),
             self.titlee("Notes", "blue"),
-            ft.TextField(label="NOTES", hint_text="- Add a note, if any"),
             self.notes,
-            ft.CupertinoFilledButton(text="Add Note", on_click=self.add_notes, padding=10, ),
-            
-            
-
+            ft.FilledTonalButton("Note", icon="add", on_click=self.add_notes),
+            self.titlee("Examples", "blue"),
+            self.examples,
+            ft.FilledTonalButton("Example", icon="add", on_click=self.add_example),
+            ft.Divider(height=10, color="red"),
+            ft.Row(
+                [ft.CupertinoFilledButton(content=ft.Text("Create Scripts", ), on_click=self.create_scripts),],
+                alignment=ft.MainAxisAlignment.CENTER,  # Alineación para distribuir los controles
+            )
         ]
         
     def titlee(self, text, color):
-        return ft.Row(
-                [ft.Text(value=text, theme_style=ft.TextThemeStyle.HEADLINE_LARGE, color=color)],
-                alignment=ft.MainAxisAlignment.CENTER,
+        return ft.Column(
+            alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                        ft.Divider(height=10, color="white"),
+                        ft.Row(
+                            [ft.Text(value=text, theme_style=ft.TextThemeStyle.HEADLINE_LARGE, color=color, ),], alignment=ft.MainAxisAlignment.CENTER,
+                               ),
+                        
+                    ]
             )
     
     def update_parameters_types(self):
@@ -319,16 +355,11 @@ class MakerMarkerDown(ft.Column):
     
     def add_parameter(self, e):
         self.update_parameters_types()
-        print(self.parameter_name.value)
-        print(self.parameter_required.value)
-        print(self.parameter_types)
-        print(self.parameter_null.value)
-        print(self.parameter_default.value)
-        print(f'checkboxes: {self.checkboxes}')
         if self.parameter_name.value and self.parameter_types:
             param = Parameter(self.parameter_name.value, 
                               self.parameter_required.value, 
                               self.parameter_types, 
+                              self.parameter_description.value,
                               self.parameter_null.value, 
                               self.parameter_default.value, 
                               self.parameter_delete)
@@ -338,14 +369,15 @@ class MakerMarkerDown(ft.Column):
             self.parameter_null.value = ""
             self.parameter_default.value = ""   
             self.parameter_types = []
+            self.parameter_description.value = ""
             self.update()
         else:
             self.alerts("Parameter Name and Type(s) are required")
             
-    def alerts(self, text):
+    def alerts(self, text, color="red"):
         self.page.snack_bar = ft.SnackBar(
             ft.Text(text),
-            bgcolor="red",
+            bgcolor=color,
         )
         self.page.snack_bar.open = True
         self.page.update()
@@ -370,66 +402,127 @@ class MakerMarkerDown(ft.Column):
         notes = Notes(self.delete_notes, self.nnotes)
         self.notes.controls.append(notes)
         self.update()
-
+    
     def delete_notes(self, e):
         self.notes.controls.remove(e)
         self.nnotes -= 1
         self.update()
     
+    def add_example(self, e):
+        example = Example(self.delete_example)
+        self.examples.controls.append(example)
+        self.update()
+
+    def delete_example(self, e):
+        self.examples.controls.remove(e)
+        self.update()
+    
+    def create_scripts(self, e):
+        example_list = []
+        notes_list = []
+        version_list = []
+        parameters_list = []
+        
+        for exa in self.examples.controls:
+            example_list.append(exa.example.value)
+        
+        for note in self.notes.controls:
+            notes_list.append(note.note.value)
+        
+        for ver in self.versions.controls:
+            version_list.append([
+                ver.display_version_name.value, 
+                ver.display_version_return.value, 
+                ver.display_version_description.value])
+        
+        for param in self.parameters.controls:
+            parameters_list.append([
+                param.display_parameter_name.value, 
+                param.display_parameter_required.value, 
+                param.display_parameter_types, 
+                param.display_parameter_null.value, 
+                param.display_parameter_default.value])
+        
+        print("title: ", self.func_title.value)
+        print("function: ", self.func_function.value)
+        print("description: ", self.func_description.value)
+        print("return: ", self.func_return.value)
+        print("parameters_list: ", parameters_list)
+        print("example_list: ", example_list)
+        print("notes_list: ", notes_list)
+        print("version_list: ", version_list)
+        
+        self.md_scripts(self.func_title.value,
+                           self.func_function.value,
+                           self.func_description.value,
+                           self.func_return.value,
+                           self.select.value,
+                           self.where.value,
+                           self.custom_usage.value,
+                           version_list,
+                           parameters_list,
+                           notes_list
+                           )
+        
+        self.ts_scripts(self.func_title.value,
+                           example_list
+                           )
+        
+        self.alerts("Creating Scripts", "green")
+        
+    def md_scripts(self, title, function, description, retorno, select, where, custom_usage, versions, parameters, notes):
+        
+        rows = '\n'.join([f"| `{p[0]}`| {'**Required**' if p[1] == "Required" else 'Optional'} | {', '.join(f'`{x.strip()}`' for x in p[2].value.split(','))} | {p[3]} | {p[4]} |" for p in parameters])
+        versions_text = f"## Versions\n{versions}" if len(versions) > 0 else ""
+        notes_text = f"### Notes\n- {notes}" if len(notes) > 0 else ""
+        
+        usage_text = f"`{title}` may be used in the query"
+        usage_text += " SELECT" if select else ""
+        usage_text += " and WHERE" if where else ""
+        usage_text += custom_usage if custom_usage else " clauses for analyzing data and applying conditional logic."
+        
+
+        info = f"""# {title}
+
+## Description
+`{function}`
+
+{description}
+
+{versions_text}
+
+### Return Type
+`{retorno}` (see [Type Conversions](/docs/QueryExpression-Type))
+
+## Parameters
+| Parameter | Required | Type(s) | Description | `null` Behavior | Default |
+| :-------- | :------- | :------ | :---------- | :-------------- | :------ |
+{rows}
+
+## Usage
+{usage_text}
+
+{notes_text}
+
+"""
+
+        # Abre el archivo en modo de escritura. Si el archivo no existe, se creará.
+        with open(f'QueryExpression-{title}.md', 'w') as f:
+            # Escribe la información en el archivo
+            f.write(info)
+
+            # Cierra el archivo
+        
+    def ts_scripts(self, title, examples):
+        pass
+    
 def main(page: ft.Page):
-    page.title = "Create Markdown scritps"
+    page.title = "Create Scritps"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.scroll = ft.ScrollMode.ADAPTIVE
 
     # create app control and add it to the page
     page.add(MakerMarkerDown())
 
-
 ft.app(main)
 
-
-# title = "GetUTMZoneBounds"
-# fucntion = "GetUTMZoneBounds(utm)"
-# description = "Converts UTM grid zone to geographic bounds."
-# retorno = "Int32"
-
-# # Define tus parámetros aquí. Cada diccionario representa una fila.
-# parametros = [
-#     {"Parameter": "param1", "Required": "Yes", "Type(s)": "int", "Description": "Este es el param1", "`null` Behavior": "N/A"},
-#     {"Parameter": "param2", "Required": "No", "Type(s)": "string", "Description": "Este es el param2", "`null` Behavior": "N/A"},
-#     # Puedes agregar más parámetros aquí...
-# ]
-
-# # Genera las filas de la tabla
-# rows = "\n".join(
-#     "| `{Parameter}` | {Required} | {Type(s)} | {Description} | {`null` Behavior} |".format(**param)
-#     for param in parametros
-# )
-
-# info = f"""# {title}
-
-# ## Description
-# `{fucntion}`
-
-# {description}
-
-# ### Return Type
-# `{retorno}` (see [Type Conversions](/docs/QueryExpression-Type))
-
-# ## Parameters
-# | Parameter | Required | Type(s) | Description | `null` Behavior |
-# | :-------- | :------- | :------ | :---------- | :-------------- |
-# {rows}
-
-# ## Usage
-# `{title}` may be used in the query SELECT and WHERE clauses for analyzing data and applying conditional logic.
-
-
-
-
-# """
-
-# # Abre el archivo en modo de escritura. Si el archivo no existe, se creará.
-# with open(f'{title}.md', 'w') as f:
-#     # Escribe la información en el archivo
-#     f.write(info)
