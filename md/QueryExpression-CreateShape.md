e0fed898324a27bf0ecdaef5d3d3a3582b1b0575
Dereck Angeles <dereckangeles15071998@gmail.com>
2024-06-20 05:00:25 -0600

    creando entornos virtuales

--- md/QueryExpression-CreateShape.md
+++ md/QueryExpression-CreateShape.md
@@ -1,7 +1,6 @@
 # CreateShape
 
 ## Description
-
 `CreateShape(multiRow, func)`
 
 Creates a closed polygon from the set of points
@@ -11,65 +10,18 @@ Creates a closed polygon from the set of points
 Creates a closed polygon from the linesets
 
 ### Return Type
-
 `ShapeSetDouble` (see [Type Conversions](/docs/QueryExpression-Type))
 
 ## Parameters
 
-| Parameter  | Required     | Type(s)               | Description                             | `null` Behavior |
-| :--------- | :----------- | :-------------------- | :-------------------------------------- | :-------------- |
-| `multiRow` | **Required** | `MultiPoint`, `Column<MultiPoint>` | The start point of the line.            | Returns `null`  |
-| `func`     | **Required** | `Point`, `Column<XY>` | The end point of the line.              | Returns `null`  |
-| `lineSet`  | **Required** | `Line` `Column<Line>` | The end point of the line.              | Divides the line at the 180th meridian. | Returns `null`  |
+| Parameter  | Required     | Type(s)                            | Description                                                 | `null` Behavior |
+| :--------- | :----------- | :--------------------------------- | :---------------------------------------------------------- | :-------------- |
+| `multiRow` | **Required** | `MultiPoint`, `Column<MultiPoint>` | This is a multipoint but it will be transfromed to a shape. | Returns `null`  |
+| `func`     | **Required** | `Point`, `Column<XY>`              | The end point of the line.                                  | Returns `null`  |
+| `lineSet`  | **Required** | `Line` `Column<Line>`              | This is a line set but it will be transfromed to a shape.   | Returns `null`  |
 
 ## Usage
-
 `CreateShape` may be used in the query SELECT clauses for analyzing data and applying conditional logic.
 
-
-```json
-{
-  "table": {
-    "name": "test/allshapes"
-  },
-  "sqlselect": ["CreateShape(allshapes)"]
-}
-```
-
-```json
-{
-  "take": 1,
-  "table": {
-    "name": "test/allshapes"
-  },
-  "sqlselect": [
-    "CreateShape(LineFromWKT('(-112.11914062674626 41.420761289004076,-105.96679687674626 33.8460869780613,-91.90429687674626 34.137572929286634)'))"
-  ]
-}
-```
-
-```sql
-SELECT CreateShape(allshapes) 
-FROM test/allshapes;
-```
-
-```sql
-SELECT CreateShape(LineFromWKT('(-112.11914062674626 41.420761289004076,-105.96679687674626 33.8460869780613,-91.90429687674626 34.137572929286634)')) 
-FROM test/allshapes
-LIMIT 1;
-```
-
-```js
-const q = ml.query();
-q.from("test/allshapes");
-q.select("CreateShape(allshapes)");
-```
-
-```js
-const q = ml.query();
-q.from("test/allshapes");
-q.select("CreateShape(LineFromWKT('(-112.11914062674626 41.420761289004076,-105.96679687674626 33.8460869780613,-91.90429687674626 34.137572929286634)'))");
-q.take(1);
-```
-
-(-89.35546875174626 45.99187335267636),(-91.64062500174623 38.38759827687156),(-79.68750000174624 40.22362624553127),(-89.35546875174626 45.99187335267636)
+### Notes
+- `CreateShape` must have at least three points.
--- md/QueryExpression-CreateShape.ts
+++ md/QueryExpression-CreateShape.ts
@@ -9,217 +9,207 @@ import type { ICodeExample } from "./controls/CodeExample/Interfaces";
 import { MultiCodeExampleViewModel } from "./controls/CodeExample/MultiCodeExampleViewModel";
 import { RaptorDashboard } from "./raptor/RaptorDashboard";
 
-export async function initModule(
-  container: HTMLElement,
-  route: ILocatedRoute
-): Promise<RaptorEngine> {
-  const dash = await new RaptorDashboard(container, route).init();
-  await ensureMonacoEditorFramework();
+export async function initModule(container: HTMLElement, route: ILocatedRoute): Promise<RaptorEngine> {
+    const dash = await new RaptorDashboard(container, route).init();
+    await ensureMonacoEditorFramework();
 
-  const vm = getViewModel(dash);
-  dash.addView(vm.generateView());
+    const vm = getViewModel(dash);
+    dash.addView(vm.generateView());
 
-  await dash.addViewModel(vm);
-  const engine = await dash.render();
+    await dash.addViewModel(vm);
+    const engine = await dash.render();
 
-  return engine;
+    return engine;
 }
 
 registerPublicDashboard({
-  id: "ext/ml-docs-query/QueryExpression-CreateShape",
-  name: "QueryExpression-CreateShape",
-  description: "CreateShape query expression documentation",
+    id: "ext/ml-docs-query/QueryExpression-CreateShape",
+    name: "QueryExpression-CreateShape",
+    description: "CreateShape query expression documentation",
 });
 
 function getViewModel(dash: RaptorDashboard): InscribedCirculeViewModel {
-  return new InscribedCirculeViewModel(dash, [
-    {
-      name: "snippetGroup1",
-      sources: [
+    return new InscribedCirculeViewModel(dash, [
         {
-          code: `{
-  "table": {
-    "name": "test/allshapes"
-  },
-  "sqlselect": ["CreateLine(XY, XY_1) as CreateLine"]
-}`,
-          language: "json",
-          label: "JSON",
-          executionMode: "query",
-          runnable: true,
-          readOnly: false,
-          syntaxHighlightingModelUri: CodeEditorMonaco.modelUris.json.query,
-        },
-        {
-          code: `SELECT CreateLine(XY, XY_1) AS CreateLine
-FROM test.allshapes;`,
-          language: "sql",
-          label: "SQL",
-          executionMode: "query",
-          runnable: true,
-          readOnly: false,
-        },
-        {
-          code: `const q = ml.query();
-q.from("test/allshapes");
-q.select("CreateLine(XY, XY_1) as CreateLine");`,
-          language: "javascript",
-          label: "JavaScript",
-          executionMode: "javascript-query",
-          runnable: true,
-          readOnly: false,
-        },
-      ],
+            name: "snippetGroup1",
+            sources: [
+                {
+                    code: `{
+    "table": {
+        "name": "test/allshapes"
     },
-    {
-      name: "snippetGroup2",
-      sources: [
-        {
-          code: `{
-  "take": 1,
-  "table": {
-    "name": "test/allshapes"
-  },
-  "sqlselect": [
-    "CreateLine(Point(-80.3128682076931, 25.645892105474), Point( -80.226219445467, 25.8652550751759), False) as CreateLine"
-  ]
+    "sqlselect": ["CreateShape(allshapes)"]
 }`,
-          language: "json",
-          label: "JSON",
-          executionMode: "query",
-          runnable: true,
-          readOnly: false,
+                    language: "json",
+                    label: "JSON",
+                    executionMode: "query",
+                    runnable: true,
+                    readOnly: false,
+                    syntaxHighlightingModelUri: CodeEditorMonaco.modelUris.json.query,
+                },
+                {
+                    code: `SELECT CreateShape(allshapes)
+FROM test.allshapes;`,
+                    language: "sql",
+                    label: "SQL",
+                    executionMode: "query",
+                    runnable: true,
+                    readOnly: false,
+                },
+                {
+                    code: `const q = ml.query();
+q.from("test/allshapes");
+q.select("CreateShape(allshapes)");`,
+                    language: "javascript",
+                    label: "JavaScript",
+                    executionMode: "javascript-query",
+                    runnable: true,
+                    readOnly: false,
+                },
+            ],
         },
         {
-          code: `SELECT CreateLine(Point(12.345453, -34.56778), Point(12.345453, -34.56778), False) AS CreateLine
+            name: "snippetGroup2",
+            sources: [
+                {
+                    code: `{
+    "take": 1,
+    "table": {
+        "name": "test/allshapes"
+    },
+    "sqlselect": [
+        "CreateShape(LineFromWKT('(-112.11914062674626 41.420761289004076,-105.96679687674626 33.8460869780613,-91.90429687674626 34.137572929286634)'))"
+    ]
+}`,
+                    language: "json",
+                    label: "JSON",
+                    executionMode: "query",
+                    runnable: true,
+                    readOnly: false,
+                },
+                {
+                    code: `SELECT CreateShape(LineFromWKT('(-112.11914062674626 41.420761289004076,-105.96679687674626 33.8460869780613,-91.90429687674626 34.137572929286634)'))
 FROM test.allshapes
 LIMIT 1;`,
-          language: "sql",
-          label: "SQL",
-          executionMode: "query",
-          runnable: true,
-          readOnly: false,
-        },
-        {
-          code: `const q = ml.query();
+                    language: "sql",
+                    label: "SQL",
+                    executionMode: "query",
+                    runnable: true,
+                    readOnly: false,
+                },
+                {
+                    code: `const q = ml.query();
 q.from("test/allshapes");
-q.select("CreateLine(Point(12.345453, -34.56778), Point(12.345453, -34.56778)) as CreateLine");
+q.select(
+    "CreateShape(LineFromWKT('(-112.11914062674626 41.420761289004076,-105.96679687674626 33.8460869780613,-91.90429687674626 34.137572929286634)'))"
+);
 q.take(1);`,
-          language: "javascript",
-          label: "JavaScript",
-          executionMode: "javascript-query",
-          runnable: true,
-          readOnly: false,
+                    language: "javascript",
+                    label: "JavaScript",
+                    executionMode: "javascript-query",
+                    runnable: true,
+                    readOnly: false,
+                },
+            ],
         },
-      ],
-    },
-  ]);
+    ]);
 }
 
-function markdownRow(
-  markdown: string,
-  s: IContainerScriptor<CodeExampleViewModel>
-): void {
-  s.row().contentTemplates((s) => {
-    s.column({ columnLg: 10, padding: { topLg: 3 } }).contentTemplates((s) => {
-      s.paragraph({ text: " " });
-      s.markdown({
-        markdown: markdown,
-      });
+function markdownRow(markdown: string, s: IContainerScriptor<CodeExampleViewModel>): void {
+    s.row().contentTemplates(s => {
+        s.column({columnLg: 10, padding: {topLg: 3}}).contentTemplates(s => {
+            s.paragraph({text: " "});
+            s.markdown({
+                markdown: markdown,
+            });
+        });
     });
-  });
 }
 
 export class InscribedCirculeViewModel extends MultiCodeExampleViewModel {
-  constructor(dash: RaptorDashboard, sources: ICodeExample[]) {
-    super(dash, sources);
-  }
+    constructor(dash: RaptorDashboard, sources: ICodeExample[]) {
+        super(dash, sources);
+    }
 
-  public onAfterRender(): void {
-    super.onAfterRender();
+    public onAfterRender(): void {
+        super.onAfterRender();
 
-    // fetch markdown content
-    ml.fetch("/ext/ml-docs-query/QueryExpression-CreateShape.md", {
-      method: "GET",
-      credentials: "include",
-    })
-      .then((resp) => resp.text())
-      .then((md) => {
-        this.markdownContent = md;
+        // fetch markdown content
+        ml.fetch("/ext/ml-docs-query/QueryExpression-CreateShape.md", {
+            method: "GET",
+            credentials: "include",
+        })
+            .then(resp => resp.text())
+            .then(md => {
+                this.markdownContent = md;
+                this.update();
+            });
+
         this.update();
-      });
+    }
 
-    this.update();
-  }
-
-  public generateView(): ViewDefinitions.IRenderingDefinition {
-    const scriptor = RSScriptor.create<CodeExampleViewModel>();
-    const s = scriptor.page("page", "page");
-    s.container({
-      fluid: true,
-      overflow: "auto",
-      //heightVH: 100,
-      customCssClasses: "ml-dash-md",
-      margin: {
-        topLg: 4,
-      },
-    }).contentTemplates((s) => {
-      s.row().contentTemplates((s) => {
-        s.column({ columnLg: 10 }).contentTemplates((s) => {
-          s.markdown({
-            markdown: "",
-            bindings: {
-              text: "markdownContent",
+    public generateView(): ViewDefinitions.IRenderingDefinition {
+        const scriptor = RSScriptor.create<CodeExampleViewModel>();
+        const s = scriptor.page("page", "page");
+        s.container({
+            fluid: true,
+            overflow: "auto",
+            //heightVH: 100,
+            customCssClasses: "ml-dash-md",
+            margin: {
+                topLg: 4,
             },
-          });
-        });
-      });
+        }).contentTemplates(s => {
+            s.row().contentTemplates(s => {
+                s.column({columnLg: 10}).contentTemplates(s => {
+                    s.markdown({
+                        markdown: "",
+                        bindings: {
+                            text: "markdownContent",
+                        },
+                    });
+                });
+            });
 
-      markdownRow(
-        `
+            markdownRow(
+                `
 ### Examples
 
 #### Creates a line from two points
 It takes to point columns from table \`test/allshapes\` and returns a LineString.
 `,
-        s
-      );
+                s,
+            );
 
-      s.row().contentTemplates((s) => {
-        s.column({
-          columnLg: 10,
-          border: { color: "secondary", width: 1, rounded: true },
-        }).contentTemplates((s) => {
-          this._codeExampleViewModels["snippetGroup1"].scriptView(
-            s,
-            "codeExampleViewModels.snippetGroup1"
-          );
-        });
-      });
+            s.row().contentTemplates(s => {
+                s.column({
+                    columnLg: 10,
+                    border: {color: "secondary", width: 1, rounded: true},
+                }).contentTemplates(s => {
+                    this._codeExampleViewModels["snippetGroup1"].scriptView(s, "codeExampleViewModels.snippetGroup1");
+                });
+            });
 
-      markdownRow(
-        `
+            markdownRow(
+                `
 
 #### Creates a line from two static points with a boolean flag
 We pass two point functions to make our values static and we pass the Boolean "False" to indicate that we do not want to split the line at the date line.
 `,
-        s
-      );
+                s,
+            );
 
-      s.row().contentTemplates((s) => {
-        s.column({
-          columnLg: 10,
-          border: { color: "secondary", width: 1, rounded: true },
-        }).contentTemplates((s) => {
-          this._codeExampleViewModels["snippetGroup2"].scriptView(
-            s,
-            "codeExampleViewModels.snippetGroup2"
-          );
+            s.row().contentTemplates(s => {
+                s.column({
+                    columnLg: 10,
+                    border: {color: "secondary", width: 1, rounded: true},
+                }).contentTemplates(s => {
+                    this._codeExampleViewModels["snippetGroup2"].scriptView(s, "codeExampleViewModels.snippetGroup2");
+                });
+            });
         });
-      });
-    });
 
-    const viewDef = scriptor.commitPage();
-    return viewDef;
-  }
+        const viewDef = scriptor.commitPage();
+        return viewDef;
+    }
 }
